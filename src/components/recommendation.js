b56efa18256be68b61270dd3216aa89f4aebfacd
utsav <utsav@relay.ai>
2020-04-11 09:41:04 +1200

    adding testimonials

--- src/components/recommendation.js
+++ src/components/recommendation.js
@@ -1,44 +1,65 @@
-import React from "react"
+import React from "react";
 
-import styled from "@emotion/styled"
-import { theme } from '../styles'
+import styled from "@emotion/styled";
+import { theme } from "../styles";
 
 const Copy = styled.h5`
-  color: ${theme.colors.dark};
+  color: ${theme.colors.light};
   margin-bottom: 20px;
-  font-family: ${theme.fonts.Roboto}
+  line-height: 28px;
+  text-align: justify;
+  padding-left: 20px;
+  padding-right: 20px;
+  font-family: ${theme.fonts.Roboto};
+`;
+
+const Info = styled.h5`
+  color: ${theme.colors.yellow};
+  margin-bottom: 10px;
+  font-family: ${theme.fonts.Roboto};
 `;
 
 const SliderContainer = styled.div`
-display: grid;
-grid-gap: 20px;
-`
+  display: flex;
+  flex-direction: column;
+  background-color: ${theme.colors.darkBlueLighter};
+  border-radius: 15px;
+  max-width: 100%;
+  padding: 20px 40px 50px 20px;
+  border: 1px solid ${theme.colors.gray};
+`;
 
 const InfoContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
- `
-  
+`;
+
 class Recommendation extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-        <SliderContainer>
-          <Copy>{this.props.message}</Copy>
-          <InfoContainer>
-            <Copy>{this.props.givenBy}</Copy>
-            </InfoContainer>
-            <InfoContainer>
-            <Copy>{this.props.designation}</Copy>
-          </InfoContainer>
-        </SliderContainer>
-      
-    )
+      <SliderContainer>
+        <InfoContainer>
+        <span style={{ color: "#FED766" }}>
+          <i class="fa fa-quote-left fa-2x" aria-hidden="true" />
+        </span>
+        <Copy>{this.props.message}</Copy>
+        <span style={{ color: "#FED766" }}>
+          <i class="fa fa-quote-right fa-2x" aria-hidden="true" />
+        </span>
+        </InfoContainer>
+        <InfoContainer>
+          <Info>{this.props.givenBy}</Info>
+        </InfoContainer>
+        <InfoContainer>
+          <Info>{this.props.designation}</Info>
+        </InfoContainer>
+      </SliderContainer>
+    );
   }
 }
 
-export default Recommendation
-
+export default Recommendation;
--- src/components/recommendations.js
+++ src/components/recommendations.js
@@ -1,28 +1,42 @@
-import React from "react"
-import styled from "@emotion/styled"
-import { Element } from 'react-scroll'
-import ScrollReveal from 'scrollreveal'
-import {  Section, theme } from '../styles'
-import Slider from "react-slick"
+import React from "react";
+import styled from "@emotion/styled";
+import { Element } from "react-scroll";
+import ScrollReveal from "scrollreveal";
+import { Section, theme, media } from "../styles";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Recommendation from './recommendation'
+import Recommendation from "./recommendation";
 
 const Title = styled.h2`
   color: ${theme.colors.dark};
-  margin-bottom: 20px;
+  margin-bottom: 40px;
+  text-align: left;
 `;
 
 const SliderContainer = styled.div`
-  width: 100%
- 
-`
+  padding-top: 90px;
+  padding-bottom: 90px;
+  margin: 0 auto;
+  width: 100%;
+  max-width: ${theme.maxWidth};
+
+  ${media.medium} {
+    padding-top: 70px;
+    padding-bottom: 70px;
+  }
+
+  ${media.small} {
+    padding-top: 60px;
+    padding-bottom: 60px;
+  }
+`;
 function SampleNextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: theme.colors.navy }}
+      style={{ ...style, display: "block", top: '100px',background: theme.colors.navy }}
       onClick={onClick}
     />
   );
@@ -33,24 +47,23 @@ function SamplePrevArrow(props) {
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: theme.colors.navy }}
+      style={{ ...style, display: "block",top: '100px', background: theme.colors.navy }}
       onClick={onClick}
     />
   );
 }
 
 class Recommendations extends React.Component {
-
   componentDidMount = () => {
-    ScrollReveal().reveal(this.refs.connect, theme.scrollRevealConfig)
-    ScrollReveal().reveal(this.refs.image, theme.scrollRevealConfig)
-  }
+    ScrollReveal().reveal(this.refs.connect, theme.scrollRevealConfig);
+    ScrollReveal().reveal(this.refs.image, theme.scrollRevealConfig);
+  };
 
-  encode = (data) => {
+  encode = data => {
     return Object.keys(data)
-        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-        .join("&");
-  }
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  };
 
   render() {
     const settings = {
@@ -65,31 +78,29 @@ class Recommendations extends React.Component {
     return (
       <Section bgColor={theme.colors.light} id="recommendations">
         <Element name="recommendations" />
-        
-            <div ref="recommendations">
-              <SliderContainer>
-                <Title>What people have to say</Title>
-                <br/>
-              <Slider {...settings}>
-                {
-                  this.props.data.map((recommendationNode, i) => {
-                    const recommendation =  recommendationNode.node;
-                    return <Recommendation
-                      key={`${recommendation.givenBy}`}
-                      message={`${recommendation.message}`}
-                      givenBy={`${recommendation.givenBy}`}
-                      designation={`${recommendation.designation}`}
-                       />
-                  })
-                } 
-                </Slider>
-              </SliderContainer>
 
-            </div>
+        <div ref="recommendations">
+          <SliderContainer>
+            <Title>Testimonials</Title>
+            <br />
+            <Slider {...settings}>
+              {this.props.data.map((recommendationNode, i) => {
+                const recommendation = recommendationNode.node;
+                return (
+                  <Recommendation
+                    key={`${recommendation.givenBy}`}
+                    message={`${recommendation.message}`}
+                    givenBy={`${recommendation.givenBy}`}
+                    designation={`${recommendation.designation}`}
+                  />
+                );
+              })}
+            </Slider>
+          </SliderContainer>
+        </div>
       </Section>
-    )
+    );
   }
 }
 
-export default Recommendations
-
+export default Recommendations;
